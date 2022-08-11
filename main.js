51f649c686684a5901164a16f2df99e0aafa33d9
alamelu98 <63118650+alamelu98@users.noreply.github.com>
2022-08-11 17:12:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var proj=document.querySelector("#projects_1").children
+
+proj[0].addEventListener("click",function()
+{
+    window.open("https://alamelu98.github.io/Bootstrap_project1/","_blank");
+
+})
+proj[1].addEventListener("click",function()
+{
+    window.open("https://mitraphoto.github.io/mitra_photography/index.html","_blank");
+
+})
+proj[2].addEventListener("click",function()
+{
+    window.open("https://alamelu98.github.io/todo-app/","_blank");
+
+})
